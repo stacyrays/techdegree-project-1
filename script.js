8a186d14ee46806dfe1ef0e645bb8bb81ac8feb8
Stacy <stacyrays@gmail.com>
2018-10-08 22:00:44 -0700

    Updated files with html template

--- script.js
+++ script.js
@@ -1,77 +1,76 @@
-//Get the HTML elements needed to perform the code
-var quotePara = document.getElementsByTagName('p')[0];
-var myButton = document.getElementsByTagName('button')[0];
-var counter = 0;
+// FSJS - Random Quote Generator
+//
+var randNum;
+var randQuote;
+var randSource;
+var randCitation;
+var randYear;
+var randBg;
 
-
-//Create the array of objects for the generator:
+// Create the array of quote objects and name it quotes
 var quotes = [
   {
     quote: "You know you are in love when you cannot fall asleep because reality is finally better than your dreams.",
-    source: "<p class='name'>Dr. Suess</div>",
+    source: "Dr. Suess",
+    citation:"Time Magazine",
+    year:"1963",
     background: "#a8d1f7",
-    fullQuote: function() {
-      return this.quote + " " + this.source;
-    }
  },
  {
    quote: "I am selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you cannot handle me at my worst, then you sure as hell do not deserve me at my best.",
-   source: "<p class='name'>Marilyn Monroe</div>",
+   source: "Marilyn Monroe",
+   citation:"New York Times",
+   year:"1957",
    background: "#cdb6fb",
-   fullQuote: function() {
-     return this.quote + " " + this.source;
-   }
  },
  {
    quote: "Get busy living or get busy dying.",
-   source: "<p class='name'>Stephen King</div>",
+   source: "Stephen King",
+   citation:"Bookworm",
+   year:"1998",
    background: "#a6fcd1",
-   fullQuote: function() {
-     return this.quote + " " + this.source;
-   }
  },
  {
    quote: "When one door of happiness closes, another opens; but often we look so long at the closed door that we do not see the one which has been opened for us.",
-   source: "<p class='name'>Helen Keller</div>",
-   background: "#f8f0b0",
-   fullQuote: function() {
-     return this.quote + " " + this.source;
-   }
+   source: "Helen Keller",
+   citation:"Washington Post",
+   year:"1960",
+   background: "#53B6E6",
  },
  {
    quote: "Love yourself first and everything else falls into line. You really have to love yourself to get anything done in this world.",
-   source: "<p class='name'>Lucille Ball</div>",
+   source: "Lucille Ball",
+   citation:"The Ed Sullivan Show",
+   year:"1955",
    background: "#f8f0b0",
-   fullQuote: function() {
-     return this.quote + " " + this.source;
-   },
  }
 ];
 
-//Set up the button event listener to run the gerRandomQuote function
-myButton.addEventListener("click", printQuote);
-
-//Function definition for printing a quote
-function printQuote() {
-  //Call out the get random quote function
-  getRandomQuote();
-
-  //Gets a random number to decide which quote object to select from the quotes array
-  function getRandomQuote(){
-    //random number formula that picks a random number between 0 - 5
-
-    var randNum = Math.floor(Math.random() * 5);
-
-    //switches the html out to new quote
-    quotePara.innerHTML = quotes[randNum].fullQuote();
-
-    //Changes body background color
-    document.body.style.backgroundColor = quotes[randNum].background;
-  }
-
+// Create the getRandomQuuote function and name it getRandomQuote
+function getRandomQuote(){
+  randNum = Math.floor(Math.random() * 5);
+  randQuote = quotes[randNum].quote;
+  randSource = quotes[randNum].source;
+  randCitation = quotes[randNum].citation;
+  randYear = quotes[randNum].year;
+  randBg = quotes[randNum].background;
+  console.log(randQuote + " " + randSource + " " + randCitation + " " + randYear + " " + randBg);
 }
-//Run the print quote function
-printQuote();
+
+// Create the printQuote function and name it printQuote
+function printQuote(){
+  getRandomQuote();
+  console.log("you clicked it");
+  document.getElementsByClassName("quote")[0].innerHTML = randQuote;
+  document.getElementsByClassName("source")[0].innerHTML = randSource + "<span class='citation'>" + randCitation + "</span><span class='year'>" + randYear + "</span>";
+  //document.getElementsByClassName("citation").innerText = randCitation;
+  //document.getElementsByClassName("year").innerText = randYear;
+  document.body.style.backgroundColor = randBg;
+}
+
+// This event listener will respond to "Show another quote" button clicks
+// when user clicks anywhere on the button, the "printQuote" function is called
+document.getElementById('loadQuote').addEventListener("click", printQuote, false);
 
 //I couldn't figure out how exactly to make the quotes change just in a setTimeOut function. So I found a for loop that counts up until 100, and each time it executes the setTimeOut code block. This link helped me understand how to get this to work; had an example: https://wsvincent.com/javascript-closure-settimeout-for-loop/
 for (var i = 1; i <= 100; i++) {
